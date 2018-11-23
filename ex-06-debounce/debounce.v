141259d4696665c8b16dd77e65fbee9cef376be6
Warren Toomey <wkt@tuhs.org>
2018-11-23 13:26:19 +1000

    Added some formal validation rules.

--- ex-06-debounce/debounce.v
+++ ex-06-debounce/debounce.v
@@ -8,7 +8,7 @@
 module	debounce(i_clk, i_btn, o_debbtn);
 
   parameter	CLOCK_RATE_HZ = 16_000_000; // 16MHz clock
-  parameter	SLOW_RATE_HZ  =  1_000_000; // 1uS clock
+  parameter	SLOW_RATE_HZ  =  1_000_000; // 1uS clock, for simulation
 
   input i_clk;				    // System clock
   input i_btn;				    // Raw button value
@@ -38,5 +38,37 @@ module	debounce(i_clk, i_btn, o_debbtn);
     // Save the button's value for the next clock tick
     last_btn <= i_btn;
   end
+
+`ifdef FORMAL
+  reg     f_past_valid;
+  initial f_past_valid = 0;
+  always @(posedge i_clk)
+    f_past_valid <= f_past_valid + 1;
+
+  // Design properties
+  
+  // The counter is bounded by the value CLOCK_RATE_HZ/SLOW_RATE_HZ
+    always @(*)
+      assert(counter <= CLOCK_RATE_HZ/SLOW_RATE_HZ);
+
+  // A button change resets the counter. We need two
+  // past values here as the counter will be zero on
+  // the clock tick after the button value differs.
+  always @(posedge i_clk)
+    if ((f_past_valid >= 2) && ($past(i_btn) != $past(i_btn,2)))
+      assert(counter == 0);
+
+  // The counter is either zero or one more than the last value
+  always @(posedge i_clk)
+    if (f_past_valid >= 1)
+      assert(counter == 0 || (counter == 1 + $past(counter)));
+
+  // The debounced value can only change when we reach the upper
+  // bound on the counter
+  always @(posedge i_clk)
+    if (f_past_valid >= 1)
+      if (o_debbtn != $past(o_debbtn))
+        assert($past(counter) == CLOCK_RATE_HZ/SLOW_RATE_HZ);
+`endif
 
 endmodule
